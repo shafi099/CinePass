7bd1e1563f4028297bed01434483ff803befa2df
Shafi Shaik <112379436+shafi099@users.noreply.github.com>
2023-04-07 12:41:03 +0530

    Add files via upload

---
+++ comingsoonscript.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const citybtns = document.querySelectorAll(".citybtn");
+    const moviehead = document.getElementById("moviehead");
+    const submit=document.getElementById("submit");
+    const moviesubmit= document.getElementById("inputmoviesubmit")
+    const nowshowing= document.getElementById("nowshowing")
+
+
+    citybtns.forEach(btn => {
+      btn.onclick = () => {
+        moviehead.innerHTML = `MOVIES AT ${btn.value.toUpperCase()} <hr>`;
+      }; 
+    });
+
+
+    submit.onclick = () => {
+        const inputval = document.getElementById("myinput").value;
+        if(inputval!=0){
+        moviehead.innerHTML = `MOVIES AT ${inputval.toUpperCase()} <hr>`;
+        }
+        else{
+            moviehead.innerHTML = `MOVIES <hr>`
+        }};    
+
+        
+    moviesubmit.onclick = () => {
+      var movieinput = document.getElementById("movienameinput").value;
+      var section = document.getElementById(movieinput.toLowerCase());
+      if (section) {
+        section.scrollIntoView({behavior: "smooth"});
+      }
+      else{
+        var bookingbox = document.getElementById("bookingbox");
+        var usesearch = document.getElementById("usesearch")
+        //var section = document.getElementById("toggles")
+        moviehead.scrollIntoView({behavior: "smooth"});
+        usesearch.innerText=""
+        bookingbox.innerHTML= `<div style="padding:200px 12px 200px 12px;"><p> '${movieinput}' is not found</p><p>To view the list of movies being screened in your city, please click on the "Now Showing" tab.</p> </div>`;
+      }
+    }
+
+/*    nowshowing.onclick = () => {
+      moviehead.scrollIntoView({behavior: "smooth"});
+      location.reload();
+    }*/
+
+/*    function moviesection() {
+      var section = document.getElementById("bookingbox");
+      section.scrollIntoView({behavior: "smooth"});
+    }*/
+  });
+  
